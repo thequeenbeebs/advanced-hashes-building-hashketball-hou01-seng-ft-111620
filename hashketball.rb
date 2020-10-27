3af7fa5a331fa8ada6c243de4ed9288cfe8c0630
Blaire Baker <bakerblaire@gmail.com>
2020-10-27 18:00:25 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -1,1 +1,13 @@
-# Write your code here!
+def game_hash
+  {:home => {
+    :team_name => "Brooklyn Nets",
+    :colors => ["black", "white"],
+    :players => []
+  },
+  :away => {
+    :team_name => "",
+    :colors => [],
+    :players => []
+    }
+  }
+end
